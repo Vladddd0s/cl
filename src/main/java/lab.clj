dd3fd41b0edd6077acadb8945ba5d6a1bfced902
Vladddd0s <vlad.antoshechkin@mail.ru>
2020-01-19 17:49:20 +0300

    change

--- src/main/java/lab.clj
+++ src/main/java/lab.clj
@@ -1,20 +1,30 @@
 (ns lab
   (:require [clojure.core.async :as a]))
 (def queue (a/chan 1 ))
-
-(defn f2 []
+(def queue1 (a/chan 1 ))
+(defn f2 [s]
   (a/take! queue
-           (fn [x] (if (not= (type x) java.lang.String)
-                     (do
-                       (a/put! queue (str x))
-                       (f2))))))
+           (fn [x]
+             ( if (not= x -1)
+               ( if (= (char 13) x)
+                 (do
+                   (println s)
+                   ( a/put! queue1 s)
+                   (f2 ""))
+                 ( do
+                   (f2 (str s x)) ) )
+               ( do
+                 (println s)
+                 (a/put! queue1 s))))))
 (defn f1 []
   (with-open [r (clojure.java.io/input-stream "C:\\\\myfile.txt")]
     (loop [c (.read r)]
       (if (not= c -1)
         (do
-          (a/put! queue (char c) )
+          (a/put! queue (char c))
+
           (recur (.read r)))
-        ( f2 )))) )
+       (a/put! queue -1) ))) )
 
 (f1)
+(f2 "")
